bb14c8c8e8a07a1f565cd357d78c5c6a0a8b5130
ParthJain07 <87576950+ParthJain07@users.noreply.github.com>
2022-01-22 22:26:53 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,67 @@
+const p1 ={
+	score: 0,
+	button: document.querySelector('#p1Btn'),
+	display: document.querySelector('#p1Display')
+}
+
+const p2 ={
+	score: 0,
+	button: document.querySelector('#p2Btn'),
+	display: document.querySelector('#p2Display'),
+}
+
+const resetBtn = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto') 
+
+let p1score = 0;
+let p2score = 0;
+let winningScore = 3; 
+let isGameOver = false;
+
+
+function updateScores(player, opponent){
+	if(!isGameOver){
+		player.score +=1;
+		if(player.score === winningScore ){
+			isGameOver = true;
+			player.display.classList.add('has-text-success');
+			opponent.display.classList.add('has-text-danger');
+			player.button.disabled=true;
+			opponent.button.disabled=true;
+			alert("Player one wins");
+		}
+		player.display.innerText = player.score;
+	}
+}
+
+
+p1.button.addEventListener('click', function() {
+	updateScores(p1, p2);
+})
+
+
+p2.button.addEventListener('click', function() {
+	updateScores(p2, p1);	
+})
+
+
+
+
+winningScoreSelect.addEventListener('change', function(){
+	winningScore = parseInt(this.value);
+	reset();
+})
+
+
+resetBtn.addEventListener('click', reset);
+
+
+function reset(){
+	isGameOver = false;
+	for(let p of ([p1,p2])){
+		p.score = 0;
+		p.display.innerText = 0;
+		p.display.classList.remove('has-text-success', 'has-text-danger');
+		p.button.disabled=false;
+	}
+}
